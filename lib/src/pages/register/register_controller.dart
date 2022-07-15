f13df5353a8283f64e654514b0bf52f6b2521b82
Louys-GA <louysgenius@gmail.com>
2022-07-15 11:01:35 -0400

    Crear usuario con imagen

--- lib/src/pages/register/register_controller.dart
+++ lib/src/pages/register/register_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:app_delivery_udemy/src/models/response_api.dart';
 import 'package:app_delivery_udemy/src/models/user.dart';
@@ -6,7 +7,7 @@ import 'package:app_delivery_udemy/src/utils/my_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-class RegisterController{
+class RegisterController {
 
   BuildContext context;
   TextEditingController emailController = new TextEditingController();
@@ -22,13 +23,13 @@ class RegisterController{
   File imageFile;
   Function refresh;
 
-  Future init(BuildContext context, Function refresh){
+  Future init(BuildContext context, Function refresh) {
     this.context = context;
     this.refresh = refresh;
     usersProvider.init(context);
   }
 
-  void register() async{
+  void register() async {
     String email = emailController.text.trim();
     String name = nameController.text;
     String lastname = lastNameController.text;
@@ -37,91 +38,98 @@ class RegisterController{
     String confirmPassword = confirmPasswordController.text.trim();
 
 
-    if (email.isEmpty || name.isEmpty || lastname.isEmpty || phone.isEmpty || confirmPassword.isEmpty){
+    if (email.isEmpty || name.isEmpty || lastname.isEmpty || phone.isEmpty ||
+        confirmPassword.isEmpty) {
       MySnackbar.show(context, 'Debes ingresar todos los campos');
       return;
     }
-    
-    if (confirmPassword != password){
+
+    if (confirmPassword != password) {
       MySnackbar.show(context, 'Las contraseñas no coincides');
       return;
     }
-    
-    if (password.length < 6){
-      MySnackbar.show(context, 'La contraseña debe tener por lo menos 6 caracteres');
+
+    if (password.length < 6) {
+      MySnackbar.show(
+          context, 'La contraseña debe tener por lo menos 6 caracteres');
       return;
     }
 
-    User user =  new User(
-      email: email,
-      name: name,
-      lastname: lastname,
-      phone: phone,
-      password: password
+    if (imageFile == null) {
+      MySnackbar.show(context, 'Selecciona una imagen');
+      return;
+    }
+
+    User user = new User(
+        email: email,
+        name: name,
+        lastname: lastname,
+        phone: phone,
+        password: password
     );
 
-    ResponseApi responseApi = await usersProvider.create(user);
+    Stream stream = await usersProvider.createWithImage(user, imageFile);
+    stream.listen((res) {
+      //ResponseApi responseApi = await usersProvider.create(user);
+      ResponseApi responseApi = ResponseApi.fromJson(json.decode(res));
+      print('RESPUESTA: ${responseApi.toJson()}');
 
-    MySnackbar.show(context, responseApi.message);
+      MySnackbar.show(context, responseApi.message);
 
-    /*
+      /*
      Hacer que la luego de registrarse se espere 3 segundo para luego volver,
      a la pantalla de login
      */
-    if (responseApi.success) {
-      Future.delayed(Duration(seconds: 3), () {
-        Navigator.pushReplacementNamed(context, 'login');
-      });
-    }
-
-    print('RESPUESTA: ${responseApi.toJson()}');
-
+      if (responseApi.success) {
+        Future.delayed(Duration(seconds: 3), () {
+          Navigator.pushReplacementNamed(context, 'login');
+        });
+      }
+    });
   }
 
-  Future selectImage(ImageSource imageSource) async{
-    pickedFile = await ImagePicker().getImage(source: imageSource);
-    if(pickedFile != null) {
-      imageFile = File(pickedFile.path);
+    Future selectImage(ImageSource imageSource) async {
+      pickedFile = await ImagePicker().getImage(source: imageSource);
+      if (pickedFile != null) {
+        imageFile = File(pickedFile.path);
+      }
+      Navigator.pop(context);
+      refresh();
     }
-    Navigator.pop(context);
-    refresh();
-  }
 
-  void showAlertDialog() {
-    Widget galleryButton = ElevatedButton(
+    void showAlertDialog() {
+      Widget galleryButton = ElevatedButton(
         onPressed: () {
           selectImage(ImageSource.gallery);
         },
         child: Text('GALERIA'),
-    );
+      );
 
-    Widget cameraButton = ElevatedButton(
-      onPressed: () {
-        selectImage(ImageSource.camera);
-      },
-      child: Text('CAMARA'),
-    );
+      Widget cameraButton = ElevatedButton(
+        onPressed: () {
+          selectImage(ImageSource.camera);
+        },
+        child: Text('CAMARA'),
+      );
 
-    AlertDialog alertDialog = AlertDialog(
-      title: Text('Selecciona tu imagen'),
-      actions: [
-        galleryButton,
-        cameraButton
-      ],
-    );
+      AlertDialog alertDialog = AlertDialog(
+        title: Text('Selecciona tu imagen'),
+        actions: [
+          galleryButton,
+          cameraButton
+        ],
+      );
 
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alertDialog;
-        }
-    );
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return alertDialog;
+          }
+      );
+    }
+
+
+    void back() {
+      Navigator.pop(context);
+    }
   }
-
-
-  
-  void back() {
-    Navigator.pop(context);
-  }
-
-}
