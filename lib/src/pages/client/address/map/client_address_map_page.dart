05901082d66b2fc38eea97b4e90bac425d949ca1
Louys <louysgenius@gmail.com>
2022-08-05 15:40:16 -0400

    Agregando widgets dentro del mapa

--- lib/src/pages/client/address/map/client_address_map_page.dart
+++ lib/src/pages/client/address/map/client_address_map_page.dart
@@ -1,4 +1,5 @@
 import 'package:app_delivery_udemy/src/pages/client/address/map/client_address_map_controller.dart';
+import 'package:app_delivery_udemy/src/utils/my_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -31,20 +32,82 @@ class _ClientAddressMapPageState extends State<ClientAddressMapPage> {
       ),
       body: Stack(
         children: [
-          _googleMaps()
+          _googleMaps(),
+          Container(
+            alignment: Alignment.center,
+            child: _iconMyLocation(),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            alignment: Alignment.topCenter,
+            child: _cardAddress(),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: _buttonAccept(),
+          )
         ],
       ),
     );
   }
 
+  Widget _cardAddress() {
+    return Container(
+      child: Card(
+        color: Colors.grey[800],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20)
+        ),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Text(
+            'Calle falsa con Carrera falsa',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonAccept() {
+    return Container(
+        height: 50,
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(vertical: 30, horizontal: 70),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text(
+              'SELECCIONAR ESTEP PUNTO'
+          ),
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)
+              ),
+              primary: MyColors.primaryColor
+          ),
+        )
+    );
+  }
+
+  Widget _iconMyLocation() {
+    return Image.asset(
+      'assets/img/my_location.png',
+      width: 65,
+      height: 65,
+    );
+  }
 
   Widget _googleMaps() {
     return GoogleMap(
       mapType: MapType.normal,
       initialCameraPosition: _con.initialPosition,
       onMapCreated: _con.onMapCreated,
-      myLocationButtonEnabled: true,
-      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
+      myLocationEnabled: false,
     );
   }
   void refresh() {
