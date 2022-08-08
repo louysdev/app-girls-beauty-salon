56359c6adf819c832fe0f81add15e7f41884b334
Louys <louysgenius@gmail.com>
2022-08-07 22:05:59 -0400

    Agregando informacion de la entrega en el mapa

--- lib/src/pages/delivery/orders/map/delivery_orders_map_page.dart
+++ lib/src/pages/delivery/orders/map/delivery_orders_map_page.dart
@@ -29,33 +29,182 @@ class _DeliveryOrdersMapPageState extends State<DeliveryOrdersMapPage> {
     return Scaffold(
       body: Stack(
         children: [
-          _googleMaps(),
           Container(
-            alignment: Alignment.bottomCenter,
-            child: _buttonAccept(),
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: _googleMaps()
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                _buttomCenterPosition(),
+                Spacer(),
+                _cardOrderInfo()
+              ],
+            ),
           )
         ],
       ),
     );
   }
 
-  Widget _buttonAccept() {
+  Widget _buttonNext() {
     return Container(
-        height: 50,
-        width: double.infinity,
-        margin: EdgeInsets.symmetric(vertical: 30, horizontal: 70),
-        child: ElevatedButton(
-          onPressed: _con.selectRefPoint,
-          child: Text(
-              'SELECCIONAR ESTE PUNTO'
-          ),
-          style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30)
+      margin: EdgeInsets.only(left: 30, right: 30, top: 5, bottom: 5),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            primary: MyColors.primaryColor,
+            padding: EdgeInsets.symmetric(vertical: 5),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12)
+            )
+        ),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                height: 40,
+                alignment: Alignment.center,
+                child: Text(
+                  'ENTREGAR PRODUCTO',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
               ),
-              primary: MyColors.primaryColor
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                  margin: EdgeInsets.only(left: 45, top: 4),
+                  height: 30,
+                  child: Icon(
+                    Icons.check_circle,
+                    color: Colors.white,
+                    size: 30,
+                  )
+              ),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _cardOrderInfo() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(20),
+          topLeft: Radius.circular(20)
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3)
+          )
+        ]
+      ),
+      child: Column(
+          children: [
+            _listTitleAddress(_con.order.address?.neighborhood, 'Barrio', Icons.my_location),
+            _listTitleAddress(_con.order.address?.address, 'Direccion', Icons.location_on),
+            Divider(color: Colors.grey[400], endIndent: 30, indent: 30),
+            _clientInfo(),
+            _buttonNext()
+          ],
+      ),
+    );
+  }
+
+  Widget _clientInfo() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+      child: Row(
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            child: FadeInImage(
+              image: _con.order.client?.image != null
+                  ? NetworkImage(_con.order.client?.image)
+                  : ('assets/img/no-image.png'),
+              fit: BoxFit.cover,
+              fadeInDuration: Duration(milliseconds: 50),
+              placeholder: AssetImage('assets/img/no-image.png'),
+            ),
           ),
-        )
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            child: Text(
+              '${_con.order.client?.name ?? ''} ${_con.order.client?.lastname ?? ''}',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16
+              ),
+              maxLines: 1,
+            ),
+          ),
+          Spacer(),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              color: Colors.grey[200]
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.phone, color: Colors.black),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _listTitleAddress(String title, String subtitle, IconData iconData) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: ListTile(
+        title: Text(
+          title ?? '',
+          style: TextStyle(
+            fontSize: 13
+          ),
+        ),
+        subtitle: Text(subtitle),
+        trailing: Icon(iconData),
+      ),
+    );
+  }
+
+  Widget _buttomCenterPosition() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        alignment: Alignment.centerRight,
+        margin: EdgeInsets.symmetric(horizontal: 5),
+        child: Card(
+          shape: CircleBorder(),
+          color: Colors.white,
+          elevation: 4.0,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Icon(
+              Icons.location_searching,
+              color: Colors.grey[600],
+              size: 20,
+            ),
+          ),
+        ),
+      ),
     );
   }
 
